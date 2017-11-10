b1afe9d1ba453361a1f5f523d53ffd236911624c
BrunoWozniak <bruno.wozniak@gmail.com>
2017-11-10 23:57:47 +0100

    Removed some duplicate code on app.js

--- src/app.js
+++ src/app.js
@@ -18,18 +18,6 @@ const store = configureStore();
 
 axios.defaults.baseURL = process.env.BACK_END_URL;
 
-// In case the user refreshes or comes back after a while
-if (localStorage.getItem('token') !== undefined) {
-	axios.get(
-		'/users/me', 
-		{ headers: { 'x-auth': localStorage.getItem('token') }}
-	).then((res) => {
-		store.dispatch(auth(res.data._id));	
-	}).catch(() => {
-		store.dispatch(deAuth());
-	});
-}
-
 const jsx = (
 	<Provider store={store}>
 		<AppRouter />
@@ -50,7 +38,7 @@ ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 // In case the user refreshes or comes back after a while
 if (localStorage.getItem('token') !== undefined) {
 	axios.get(
-		'http://localhost:3000/users/me', 
+		'/users/me', 
 		{ headers: { 'x-auth': localStorage.getItem('token') }}
 	).then((res) => {
 		store.dispatch(auth(res.data._id));
